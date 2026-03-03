b06b644e6d109a3abd2b464088b3e35feb38b996
NataliaKlimaszewska <natalia.klimaszewska@studenci.collegiumwitelona.pl>
2026-03-03 10:19:58 +0100

    base page

---
+++ pokiepaws/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-blue-500">
+            <h1 className="text-4xl font-bold text-white shadow-lg p-4 bg-black/20 rounded-xl">
+                Tailwind 4 Działa! 🐾  :3
+            </h1>
+        </div>
+    );
+}
